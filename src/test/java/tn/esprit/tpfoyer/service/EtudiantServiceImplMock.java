8a3f569318e1bc3abd8cabf3698c7a33b3c0bbe8
BALLAMOUSSAKEITA <ballamoussa.keita@esprit.tn>
2024-10-16 16:08:35 +0100

    correction test

--- src/test/java/tn/esprit/tpfoyer/service/EtudiantServiceImplMock.java
+++ src/test/java/tn/esprit/tpfoyer/service/EtudiantServiceImplMock.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class EtudiantServiceImplTest {
+
+public class EtudiantServiceImplMock {
 }
